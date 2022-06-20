55db1ec22cdc98c33a666afb6daa2bd809f07374
namsh1125 <namsh1125@naver.com>
2022-06-20 22:18:36 +0900

    solve problem

--- SWEA_1859.java
+++ SWEA_1859.java
@@ -1,3 +1,9 @@
+/*
+    SW Expert Academy 1859. 백만장자 프로젝트
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LrsUaDxcDFAXc
+
+    앞에서부터 푸는 것이 아니라 뒤를 보고 풀어야 했던 문제.
+ */
 import java.util.Scanner;
 
 public class SWEA_1859 {
@@ -7,41 +13,32 @@ public class SWEA_1859 {
         int T;
         T = sc.nextInt(); // Test case
 
-        for (int i = 0; i < T; i++) {
+        for (int test_case = 0; test_case < T; test_case++) {
             int N; // Day
             N = sc.nextInt();
 
             // 변수 선언
             int[] price = new int[N]; // 매매가를 저장할 N개의 공간 생성
-            int benefit = 0; // 이익
-            int max = 0, nextMax = 0; // 최대 값의 위치와 그 다음 최대 값의 위치를 저장할 변수 생성
+            long benefit = 0; // 이익
+            int max = 0; // 최댓값
 
+            // 매매가 입력받기
             for (int j = 0; j < N; j++) {
                 price[j] = sc.nextInt();
             }
 
-            while (max - nextMax != 1) { // End condition
-
-                // 가장 비싼 날과 다음으로 비싼 날 찾기
-                for (int j = max; j < price.length; j++) {
-                    if (price[j] > price[max]) {
-
-                        // 가장 비싼 날에 팔기
-                        for (int k = j; j < max; j++) {
-                            benefit = benefit + (price[max] - price[j]);
-                        }
-
-                        nextMax = max;
-                        max = j;
-
-                    }
+            // 이익 계산
+            for (int j =N-1;j>=0;j--){
+                if(price[j] > max){
+                    max = price[j];
                 }
-
-
-
-                System.out.println(benefit);
+                else {
+                    benefit = benefit + (max - price[j]);
+                }
             }
 
+            // 출력
+            System.out.println(String.format("#%d %d", test_case, benefit));
         }
 
     }
