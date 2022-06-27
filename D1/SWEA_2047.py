eb5e34c0df842175eaba5b514d8ab2e379f0af23
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-06-28 00:56:45 +0900

    solve problem

---
+++ D1/SWEA_2047.py
@@ -0,0 +1,12 @@
+'''
+    SW Expert Academy 2047. 신문 헤드라인
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QKsLaAy0DFAUq&categoryId=AV5QKsLaAy0DFAUq&categoryType=CODE&problemTitle=&orderBy=INQUERY_COUNT&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=1
+    Python의 문자열 관련 Method에 대해 알 수 있었던 문제
+      - .upper : 모든 알파벳을 대문자로
+      - .lower : 모든 알파벳을 소문자로
+      - .isupper : 모든 문자열이 대문자인지 판별
+      - .islower : 모든 문자열이 소문자인지 판별
+'''
+
+string = input()
+print(string.upper())
