daddb78da0c74ce2cbf5e4067021c4b09fc966ee
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-06-27 11:53:50 +0900

    try to solve problem

--- SWEA_1244.java
+++ SWEA_1244.java
@@ -7,98 +7,82 @@
     입력 받은 숫자를 한 자리씩 쪼개 배열에 저장하는 방법에 대해 알 수 있었던 문제 (int -> int[])
       - 숫자를 String으로 바꾸고, Character.getNumericValue method를 사용하여 저장
       - 배열 = Integer.toString(숫자).chars().map(c -> c - '0').toArray();
-    DFS에 관해 공부해 볼 수 있었던 문제
+    DFS에 관해 공부해 볼 수 있었던 문제 (https://better-tomorrow.tistory.com/entry/DFS-BFS-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0)
+    StringBuffer에 대해 다시한번 알 수 있었던 문제
 
+    코드 참고: https://c3epmos.tistory.com/136
  */
 
-// Todo: DFS 코드 읽고 작성
+// Todo: 나머지 변경 횟수 처리
 
 import java.util.Scanner;
 
-public class SWEA_1244 {
+public class Solution {
+    public static int[] num;
+    public static int max;
+
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
 
         for (int test_case = 1; test_case <= T; test_case++) {
-            int number, change, repeat = -1;
-            int[] num;
+            int number, change;
+            max = 0;
 
             // 숫자를 받아 배열에 쪼개서 저장하기
             number = sc.nextInt(); // 숫자 받기
-            num = new int[(int) Math.log10(number) + 1]; // 숫자를 나눠 저장할 공간 생성
-
             num = Integer.toString(number).chars().map(c -> c - '0').toArray(); // int -> int[]
 
             // 교환 횟수 입력받기
             change = sc.nextInt();
 
-            // 자리 바꾸기
-            // 큰 값은 앞으로, 작은 값은 뒤로
-            // 같은 숫자를 가지고 있는 경우 가장 뒤에 있는 숫자를 가져오기
-
-            // Todo: 88823, 88832 전부 2번의 교환을 통해 이뤄짐. 같은 교환 내에서 최대를 구하는 방법을 찾아야 함
-            for (int i = 0; i < num.length && change > 0; i++) { // 범위 내에서, 교환 횟수가 있을 때
-                int max = i;
-
-                // 가장 큰 값 찾기
-                for (int j = i; j < num.length; j++) {
-                    if (num[max] < num[j]) {
-                        max = j;
-                        System.out.println("we found max number" + max);
-                    }
-                    else if (max!=i && num[max] == num[j]) {
-                        repeat = j;
-                        System.out.println("we found repeat number");
-                    }
-
-                }
-
-                // 자리 바꾸기
-                // 근데 중복되는 숫자가 있다면, 마지막 숫자와 바꾸기
-                if (i != max) {
-
-                    if (repeat != -1) { // 중복되는 숫자가 있는 경우, 가장 뒤에 있는 숫자와 바꾸기
-                        swap(num, i, repeat);
-                    } else { // 중복되는 숫자가 없는 경우
-                        swap(num, i, max);
-                    }
-
-                    // 교환 횟수 감소
-                    change--;
-                }
-            }
-
-
-            // 교환 횟수가 남은 경우
-            // 횟수가 짝수면 그대로 홀수면 십의 자리와 일의 자리 교환
-            // 반복하는 값이 있는 경우 패스
-            if (change > 0 && repeat == -1) {
-                change = change % 2;
-
-                // 남은 교환 횟수가 홀수인 경우
-                if (change == 1) {
-                    swap(num, num.length - 2, num.length - 1);
-                }
-
-            }
-
-            // 배열 안에 저장된 값을 하나로 모으기
-            String value = "";
-            for (int i = 0; i < num.length; i++)
-                value = value + Integer.toString(num[i]);
+            // 최댓값 구하기
+            dfs(0, 0, change);
 
             // 출력하기
-            System.out.println(String.format("#%d %s", test_case, value));
-
+            System.out.println(String.format("#%d %d", test_case, max));
         }
     }
 
-    private static void swap(int[] num, int a, int b) {
-        int temp;
-        temp = num[a];
-        num[a] = num[b];
-        num[b] = temp;
+    // dfs 함수 (인덱스, 깊이, 변경 가능 흿수)
+    public static void dfs(int index, int cnt, int change){
+        // 다 움직였다면
+        if(change == cnt) {
+
+            // 현재 배열에 저장되어 있는 값을 합치고 저장되어 있는 최댓값과 비교
+            StringBuffer sb = new StringBuffer();
+            for(int i : num)
+                sb.append(i);
+
+            max = Math.max(max, Integer.parseInt(sb.toString()));
+            return;
+        }
+
+        //전 범위 탐색 (시작 인덱스 ~ 끝)
+        for(int i = index ; i < num.length ; i++) {
+
+            // 전 범위 탐색(시작 + 1 ~ 끝)
+            for (int j = i + 1; j < num.length; j++) {
+
+                // 뒤에 큰 수가 있다면 (순서를 바꾸는 경우)
+                if (num[i] <= num[j]) {
+
+                    // swap
+                    int temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+
+                    // dfs 함수를 재귀 호출하여 더 바꿀수 있는지 확인 (깊이 1 추가)
+                    dfs(i, cnt + 1, change);
+
+                    // 처음에 받았던 값으로 되돌리기
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
     }
 }
