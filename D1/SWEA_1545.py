b553b11a57221f34fb5a01803ed2e98f03879808
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-06-28 00:33:57 +0900

    solve problem

---
+++ D1/SWEA_1545.py
@@ -0,0 +1,11 @@
+'''
+    SW Expert Academy 1545. 거꾸로 출력해 보아요
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV2gbY0qAAQBBAS0&categoryId=AV2gbY0qAAQBBAS0&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=2
+    Python에서 띄어 써서 출력하는 방법에 대해 알 수 있었던 문제
+'''
+
+N = int(input())
+
+while N>=0:
+    print(N, end=' ')
+    N = N-1
