15ba4f5cd14153442dd4147d6245954f6bf57535
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-06-29 16:09:49 +0900

    solve problem

---
+++ D1/SWEA_2043.py
@@ -0,0 +1,8 @@
+'''
+    SW Expert Academy 2043. 서랍의 비밀번호
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QJ_8KAx8DFAUq&categoryId=AV5QJ_8KAx8DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=2
+    이번 문제를 통해 얻은 게 없음.
+'''
+
+P, K = map(int, input().split())
+print(P - K + 1)
