7871e5c7c5ed31fff88763e65ae14088ba52bf88
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-07-02 10:55:44 +0900

    solve problem

---
+++ D1/SWEA_2027.py
@@ -0,0 +1,15 @@
+'''
+    SW Expert Academy 2027. 대각선 출력하기
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QFuZ6As0DFAUq&categoryId=AV5QFuZ6As0DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=2
+    
+    이번 문제를 통해 얻은 게 없음.
+    
+    < comment >
+    출제자의 의도는 반복문을 이용해서 출력해 봐라 같은데 그렇게 되면 시간복잡도가 높아져 그냥 print를 사용해 푼 문제
+'''
+
+print('#++++')
+print('+#+++')
+print('++#++')
+print('+++#+')
+print('++++#")
