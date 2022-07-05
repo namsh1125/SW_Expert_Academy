0e6d23e18e38bdd39de7b21cef833e8055979e2e
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-07-05 14:33:03 +0900

    solve problem

---
+++ D1/SWEA_2019.py
@@ -0,0 +1,12 @@
+'''
+    SW Expert Academy 2019. 더블더블
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QDEX6AqwDFAUq&categoryId=AV5QDEX6AqwDFAUq&categoryType=CODE&problemTitle=&orderBy=INQUERY_COUNT&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=2
+    
+    이번 문제를 통해 얻은 게 없음.
+    
+'''
+
+T = int(input())
+
+for i in range(0, T+1):
+    print(pow(2, i), end=' ');
