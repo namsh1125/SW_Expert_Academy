9ebb9235742d442e676634d33a87753257612551
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-07-13 23:46:21 +0900

    solve problem

---
+++ D1/SWEA_1938.py
@@ -0,0 +1,12 @@
+'''
+    SW Expert Academy 1938. 아주 간단한 계산기
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5PjsYKAMIDFAUq&categoryId=AV5PjsYKAMIDFAUq&categoryType=CODE&problemTitle=&orderBy=INQUERY_COUNT&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=2
+    이번 문제를 통해 얻은 게 없음.
+'''
+
+a, b = map(int, input().split())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
