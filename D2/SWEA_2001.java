87f9ffd74b71ef3ec8d2ef3ddabb55877f21d316
SeungHyeon <45845547+namsh1125@users.noreply.github.com>
2022-07-21 21:50:16 +0900

    solve problem

--- D2/SWEA_2001.java
+++ D2/SWEA_2001.java
@@ -1,16 +1,17 @@
 /*
     SW Expert Academy 2001. 파리 퇴치
-    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&problemLevel=2&contestProbId=AV5PzOCKAigDFAUq&categoryId=AV5PzOCKAigDFAUq&categoryType=CODE&problemTitle=&orderBy=INQUERY_COUNT&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1
+    https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PzOCKAigDFAUq&categoryId=AV5PzOCKAigDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1
 
-    10개의 test case 중 5개 정답
+    끝에서 M개라면 +1 해줘야 한다는 사실을 다시 상기한 문제
+    ( 끝이 [4], 뒤에서 2개면 [4], [3]. 4 - 2인 2부터가 아니라. )
  */
 
 import java.util.Scanner;
 
 class Solution {
     public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
         for (int test_case = 1; test_case <= T; test_case++) {
@@ -18,7 +19,7 @@ class Solution {
             int N = sc.nextInt();
             int M = sc.nextInt();
 
-            // 파리의 개수 입력 받기
+            // 파리의 개수 입력받기
             int[][] fly = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -27,13 +28,10 @@ class Solution {
             }
 
             int max = 0;
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < N - M + 1; i++) {
+                for (int j = 0; j < N - M + 1; j++) {
 
-                for (int j = 0; j < N; j++) {
-
-                    if (i + M >= N || j + M >= N) //overflow
-                        continue;
-
+                    // 공간 내 파리의 개수 구하기
                     int sum = 0;
 
                     for (int k = i; k < i + M; k++) {
@@ -42,12 +40,12 @@ class Solution {
                         }
                     }
 
+                    // 최댓값 비교
                     if (max < sum)
                         max = sum;
 
                 }
             }
-
 
             // 출력
             System.out.println(String.format("#%d %d", test_case, max));
